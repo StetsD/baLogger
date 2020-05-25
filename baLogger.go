c4da675bb162f17cd09f2453e77f6658ef1458c6
stdm <must.be.std@gmail.com>
2020-05-25 11:04:06 +0300

    Flag && Bench methods impl

--- baLogger.go
+++ baLogger.go
@@ -1,21 +1,44 @@
 package baLogger
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Decorator struct {
 	types []string
+	f     func()
+	flag  string
+}
+
+func (d *Decorator) Call() {
+	d.f()
 }
 
 func (d *Decorator) Flag(flag string) *Decorator {
+	prevFunc := d.f
+	d.flag = flag
+	d.f = func() {
+		fmt.Printf("START : %s\n", flag)
+		prevFunc()
+		fmt.Printf("END : %s\n", flag)
+	}
+	return d
+}
+
+func (d *Decorator) Bench() *Decorator {
+	prevFunc := d.f
+
+	d.f = func() {
+		start := time.Now()
+		prevFunc()
+		fmt.Printf("Timer : %s %s\n", d.flag, time.Since(start))
+	}
 
 	return d
 }
 
-func (d *Decorator) Bench(timeVal time.Time) *Decorator {
-	return d
-}
-
-func Log(f func()) Decorator {
-	decorator := Decorator{}
+func Log(f func()) *Decorator {
+	decorator := &Decorator{make([]string, 0), f, ""}
 	return decorator
 }
--- test/test.go
+++ test/test.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"baLogger"
+	"time"
+)
 
+func something(a, b int) int {
+	time.Sleep(2 * time.Second)
+	return a + b
+}
+
+func main() {
+	baLogger.Log(func() { something(2, 2) }).Flag("Mazafaka").Bench().Call()
 }
